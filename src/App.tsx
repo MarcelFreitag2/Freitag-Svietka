a900fa0f50b3c13fa825048a55dbc56c21231bb2
sveta22844 <tatarchenkosveta24@gmail.com>
2026-03-12 22:04:55 +0100

    zmiany w niektorych inputach, dodanie funkcji platnosci

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import type {SyntheticEvent} from "react";
+import {type ChangeEvent, type SyntheticEvent, useState} from "react";
 import './Logic.ts'
 import {handleImie, handleKapok, handleRegulamin} from "./Logic.ts";
 import { handleWybor } from "./Logic.ts";
@@ -10,12 +10,17 @@ function App() {
     function handleSubmit(event: SyntheticEvent<HTMLFormElement>) {
     console.log("Submitting", event);
     }
+    function handlePaymentChange(e: ChangeEvent<HTMLInputElement>) {
+        setPaymentMethod(e.target.value);
+        console.log(e.target.value)
+    }
 
+    const [paymentMethod, setPaymentMethod] = useState("blik");
 
   return (
     <>
         <div>
-            <h1>⚓Mazurska Przystań⚓</h1>
+            <h2>⚓Mazurska Przystań⚓</h2>
             <form onSubmit={handleSubmit}>
                 <input
                     type="text"
@@ -33,28 +38,70 @@ function App() {
                 <option value={"omega"}>Flagowa Omega</option>
                 </select>
                 <br></br>
-                Czas wypożyczenia(w godzinach):
-                <input
-                   type="range"
-                   min="1"
-                   max="8"
-                   step="0.5"
-                   value= ""
-                   onChange={handleSuwak}
-                ></input>
+                <h1>Czas wypożyczenia(w godzinach):</h1>
+                <input type="range" min="0" max="8" step="0.1" value="0.4"
+                   onChange={handleSuwak} >
+                </input>
                 <br></br>
-                    Opcjonalnie:<br/>
+                    Opcjonalnie:
                 <br/>
-                    Kapok dla dziecka:
+                <div id={"check"}>
+                    <h1>Kapok dla dziecka:</h1>
                     <input type={"checkbox"} name={"kapok"} onChange={handleKapok} />
-                    <br/>
-                    Opieka Instruktora:
+                </div>
+                <div id={"check"}>
+                    <h1>Opieka Instruktora:</h1>
                     <input type={"checkbox"} name={"instru"} onChange={handleKapok} />
+                </div>
+                <div id={"check"}>
+                <h1>Zaakceptuj Regulamin:</h1>
+                <input type={"checkbox"} name={"regulamin"} onChange={handleRegulamin} />
+                </div>
+                    <br/>
+                <div id="licz">miejsce na automatyczne przelicznie
+                hffhfhfh fjjffj
+                </div>
                 <br></br>
-                Zaakceptuj Regulamin:
-                <input type={"checkbox"} name={"regulamin"} onChange={handleRegulamin}/>
-                <br/>
                 <button type="submit">Wylicz Cenę</button>
+
+                <div>
+                    <p>Wybierz formę płatności:</p>
+                    <label>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="blik"
+                            checked={paymentMethod === "blik"}
+                            onChange={handlePaymentChange}
+                        />
+                        BLIK
+                    </label>
+
+
+                    <label style={{ marginLeft: "15px" }}>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="card"
+                            checked={paymentMethod === "card"}
+                            onChange={handlePaymentChange}
+                        />
+                        Karta
+                    </label>
+
+                    <label style={{ marginLeft: "15px" }}>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="cash"
+                            checked={paymentMethod === "cash"}
+                            onChange={handlePaymentChange}
+                        />
+                        Gotówka
+                    </label>
+                </div>
+
+
             </form>
         </div>
     </>
