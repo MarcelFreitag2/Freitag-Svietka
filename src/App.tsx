2a5ab6b7909bc0dde55c37da2d99f9aa86d1344e
MarcelFreitag2 <marcel.freitag@cyfroweszkoly.pl>
2026-03-12 22:19:28 +0100

    funkcja obliczeniowa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import {type ChangeEvent, type SyntheticEvent, useState} from "react";
+import './Kalkulator'
+import {refresh} from "./Kalkulator.ts";
 
 function App() {
 
     const [textImie, setTextImie] = useState("")
-    const [selectWypozy, setSelectWypozy] = useState("")
-    const [intSuwake, setIntSuwake] = useState("")
+    const [selectWypozy, setSelectWypozy] = useState("kajak")
+    const [intSuwake, setIntSuwake] = useState("5")
+    const [kapok, setKapok] = useState("off")
+    const [opieka, setOpika] = useState("off")
+    const [suma, setsuma] = useState<number>(refresh(selectWypozy, intSuwake, kapok, opieka))
 
     function handleSubmit(event: SyntheticEvent<HTMLFormElement>) {
         console.log("Submitting", event);
@@ -17,20 +22,48 @@ function App() {
 
     function handleWybor(e: ChangeEvent<HTMLSelectElement>) {
         setSelectWypozy(e.target.value)
+        const chwilowyWybor = e.currentTarget.value
+        setsuma(refresh(chwilowyWybor, intSuwake, kapok, opieka))
     }
 
     function handleSuwak(e: ChangeEvent<HTMLInputElement>) {
         setIntSuwake(e.currentTarget.value)
+        const chwilowySuwak = e.currentTarget.value
+        setsuma(refresh(selectWypozy, chwilowySuwak, kapok, opieka))
     }
 
-    function handleKapok(e: ChangeEvent<HTMLInputElement>) {
-        console.log("handleSuwak", e);
+    function handleKapok() {
+        let chwilowykapok = kapok
+        if(kapok == "off")
+        {
+            setKapok("on")
+            chwilowykapok = "on"
+        }
+        else
+        {
+            setKapok("off")
+            chwilowykapok = "off"
+        }
+        setsuma(refresh(selectWypozy, intSuwake, chwilowykapok, opieka))
+    }
+    function handleOpieka()
+    {
+        let chwilowyOpieka = opieka
+        if(opieka == "off") {
+            setOpika("on")
+            chwilowyOpieka = "on"
+        }
+        else
+        {
+            setOpika("off")
+            chwilowyOpieka = "off"
+        }
+        setsuma(refresh(selectWypozy, intSuwake, kapok, chwilowyOpieka))
     }
 
     function handleRegulamin(e: ChangeEvent<HTMLInputElement>) {
         console.log("handleSuwak", e);
     }
-
 
   return (
     <>
@@ -48,7 +81,7 @@ function App() {
                     value = {selectWypozy}
                     onChange={handleWybor}
                 >
-                <option value="kajak">Kajak</option>
+                <option value={"kajak"}>Kajak</option>
                 <option value={"rowerwodny"}>Rower wodny</option>
                 <option value={"omega"}>Flagowa Omega</option>
                 </select>
@@ -58,7 +91,7 @@ function App() {
                    type="range"
                    min="1"
                    max="8"
-                   step="0.5"
+                   step="1"
                    value= {intSuwake}
                    onChange={handleSuwak}
                 ></input>
@@ -67,15 +100,16 @@ function App() {
                 <br></br>
                     Opcjonalnie:<br/>
                     Kapok dla dziecka:
-                    <input type={"checkbox"} name={"kapok"} onChange={handleKapok} />
+                    <input type={"checkbox"} name={"kapok"} onChange={handleKapok}/>
                     <br/>
                     Opieka Instruktora:
-                    <input type={"checkbox"} name={"instru"} onChange={handleKapok} />
+                    <input type={"checkbox"} name={"instru"} onChange={handleOpieka} />
                 <br></br>
                 Zaakceptuj Regulamin:
                 <input type={"checkbox"} name={"regulamin"} onChange={handleRegulamin}/>
                 <br/>
             </form>
+            {suma}
         </div>
     </>
   )
